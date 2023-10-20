b3af9c64be2ef298c2b6456eb870b3083ea8316a
KamLynch <kambrialynch95@gmail.com>
2023-10-19 22:46:09 -0400

    Completed codewars challenge and solution using PREP

---
+++ CC 2023-10-19/make_uppercase.js
@@ -0,0 +1,15 @@
+/* Write a function which converts the input string to uppercase. */
+
+//Parameter: take in a string
+//Result: the string in all uppercase
+//Example:
+ //   assert.strictEqual(makeUpperCase(""),           "");
+   // assert.strictEqual(makeUpperCase("hello"), "HELLO");
+  //  assert.strictEqual(makeUpperCase("Hello"), "HELLO");
+  //  assert.strictEqual(makeUpperCase("HELLO"), "HELLO");
+//Psuedocode:
+
+
+function makeUpperCase(str) {
+ return str.toUpperCase()
+}
